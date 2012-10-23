fe3064c9aff31c806666ca90d28f3a486a349985
Eric Hamilton <dilvie@dilvie.com>
2012-10-23 12:05:23 -0400

    Simple Express server

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require("express"),
-    app     = express.createServer();
-    
-app.get("/", function(req, res) {
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 4567,
+    publicDir = process.argv[2] || __dirname + '/public';
+
+app.get("/", function (req, res) {
   res.redirect("/index.html");
 });
 
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
-  }));
-  app.use(app.router);
-});
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
 
-app.listen(4567);
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);

3f2694a198016dd0b039daa4855b9a9f6d1a9183
Nader Chehab <naderchehab@gmail.com>
2015-04-01 08:54:27 -0700

    dev in progress

--- public/js/snake.js
+++ public/js/snake.js
@@ -1,15 +1,49 @@
 (function() {
-    var canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
 
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
-    resizeCanvas();
+    var canvas = document.getElementById('canvas')
+        , ctx = canvas.getContext('2d')
+        , queue = []
+        , snake = {
+            x: 30
+            , y: 30
+            , direction: 'right'
+            , speed: 0.6
+            , size: 5
+            , blockSize: 2
+        };
+
+    init();
+
+    function init() {
+        window.addEventListener('resize', resizeCanvas, false);
+        window.addEventListener('keydown', keyDown, false);
+        resizeCanvas();
+        queue.push({});
+    }
 
     function resizeCanvas() {
         canvas.height = window.innerHeight * 0.95;
         canvas.width =  Math.min(canvas.height * 1.5, document.body.clientWidth);
         draw();
+    }
+
+    function keyDown(e) {
+        e = e || window.event;
+        switch(e.keyCode) {
+            case 38:
+                snake.direction = 'up';
+            break;
+            case 40:
+            snake.direction = 'down';
+            break;
+            case 37:
+                snake.direction = 'left';
+            break;
+            case 39:
+                snake.direction = 'right';
+            break;
+
+        }
     }
 
     function rectPercent(x, y, width, height) {
@@ -19,14 +53,50 @@
         ctx.closePath();
     }
 
-    var x = 0;
-
     function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        rectPercent(x, 20, 2, 3);
+        var xdir = 0
+            , ydir = 0;
 
-        if (x < 98) {
-            x += 1;
+        switch(snake.direction) {
+            case 'left':
+                xdir = -1;
+                ydir = 0;
+                break;
+            case 'right':
+                xdir = 1;
+                ydir = 0;
+                break;
+            case 'up':
+                xdir = 0;
+                ydir = -1;
+                break;
+            case 'down':
+                xdir = 0;
+                ydir = 1;
+                break;
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (var i = 0; i < snake.size; i++) {
+            rectPercent(snake.x-(i*xdir*snake.blockSize), snake.y-(i*ydir*snake.blockSize), snake.blockSize, snake.blockSize);
+        }
+
+        if (xdir !== 0) {
+            if (snake.x < 100 + snake.size * snake.blockSize) {
+                snake.x += xdir * snake.speed;
+            }
+            else {
+                snake.x = 0;
+            }
+        }
+        else if (ydir !== 0) {
+            if (snake.y < 100 + snake.size * snake.blockSize) {
+                snake.y += ydir * snake.speed;
+            }
+            else {
+                snake.y = 0;
+            }
         }
 
         window.requestAnimationFrame(draw);

b715e14fdb3337fa7c5ccf9c5e2b3655d9ee6a5a
Nader Chehab <naderchehab@gmail.com>
2015-04-07 23:52:08 -0700

    Removed hostname

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
-    hostname = process.env.HOSTNAME || 'localhost',
     port = parseInt(process.env.PORT, 10) || 4567,
     publicDir = process.argv[2] || __dirname + '/public';
 
@@ -22,5 +21,5 @@ app.use(errorHandler({
   showStack: true
 }));
 
-console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
-app.listen(port, hostname);
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, port);
+app.listen(port);
